e42e5bad7b8bf8ddd25233afc4d3d78329b29994
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2023-11-07 22:51:29 +0200

    fix(housekeeping): Cleared some logic from baseMap component to be more generic

--- src/components/BaseComponents/BaseMap.tsx
+++ src/components/BaseComponents/BaseMap.tsx
@@ -1,22 +1,21 @@
 import { MapContainer, TileLayer } from "react-leaflet";
-import { CONFIG } from "@/common/mapSettings";
-import { PropsWithChildren } from "react";
 
-export default function BaseMap(props: PropsWithChildren) {
-    const zoomLevel = CONFIG.default_zoom_level;
-    const defaultCenter = CONFIG.center;
+export default function BaseMap(props: mapLeaflet) {
     return (
         <MapContainer
-            center={defaultCenter}
-            zoom={zoomLevel}
+            center={props.center}
+            zoom={props.zoom}
             scrollWheelZoom={false}
+            maxBounds={props.maxBounds}
+            maxBoundsViscosity={1}
+            minZoom={props.minZoom}
             className="w-full h-screen map"
         >
         <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Mapbox</a> contributors'
-        url="https://api.mapbox.com/styles/v1/virtuosofriend/clobmjbzm013j01qmbjlof6g0/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmlydHVvc29mcmllbmQiLCJhIjoiY2lwY2lvc3ZjMDAyNnZobm5haDZ1M2VrcSJ9.IGVbY93V5cF7KvEVJnmUTQ"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Mapbox</a> contributors'
+            url="https://api.mapbox.com/styles/v1/virtuosofriend/clobmjbzm013j01qmbjlof6g0/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmlydHVvc29mcmllbmQiLCJhIjoiY2lwY2lvc3ZjMDAyNnZobm5haDZ1M2VrcSJ9.IGVbY93V5cF7KvEVJnmUTQ"
         />
-        {props.children}
-    </MapContainer>
+            {props.children}
+        </MapContainer>
     );
 }
