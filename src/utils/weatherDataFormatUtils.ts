7b263ff409e499bae009786a2b043509d3328774
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2024-03-24 09:17:49 +0200

    task(stationModal): Removed the literals for old weather description. Openweather API provides better literals. FWM-48

--- src/utils/weatherDataFormatUtils.ts
+++ src/utils/weatherDataFormatUtils.ts
@@ -1,52 +1,5 @@
 import { WeatherData } from "@/types";
 
-type literalOptions = {
-    [key: string]: string;
-};
-
-/**
- * @todo Propably move these literals to some place better. Maybe i18n?
- */
-const literals: literalOptions = {
-    cloudy: "Cloudy",
-    cold: "Cold!",
-    dreary_overcast: "Dreary overcast",
-    flurries: "Flurries",
-    fog: "Fog",
-    freezing_rain: "Freezing rain",
-    hazy_sunshine: "Hazy sunshine",
-    hot: "Hot!",
-    ice: "Icy conditions",
-    intermittent_clouds: "Intermittent clouds",
-    light_rain: "Light rain",
-    mostly_cloudy_w_flurries: "Mostly cloudy with flurries",
-    mostly_cloudy_w_showers: "Mostly cloudy with showers",
-    mostly_cloudy_w_snow: "Mostly cloudy with snow",
-    "mostly_cloudy_w_t-storms": "Mostly cloudy with thunderstorms",
-    mostly_cloudy: "Mostly cloudy",
-    mostly_sunny: "Mostly sunny",
-    partly_cloudy_w_flurries: "Partly loudy with flurries",
-    partly_cloudy_w_showers: "Partly loudy with showers",
-    "partly_cloudy_w_t-storms": "Partly loudy with thunderstorms",
-    partly_sunny: "Partly sunny",
-    rain_and_snow: "Rain & snow",
-    rain: "Rain",
-    showers: "Showers",
-    sleet: "Sleet",
-    snow: "Snow",
-    "t-storms": "Thunderstorms",
-    thunderstorm: "Thunderstorm",
-    windy: "Windy",
-    clear: "Clear night",
-    sunny: "Sunny",
-    mostly_clear: "Mostly clear",
-    hazy_moonlight: "Hazy moonlight",
-};
-
-export const weatherConditionsText = (text: string): string => {
-    return literals[text];
-};
-
 export const formatDate = (inputDate: Date): string => {
     const options: Intl.DateTimeFormatOptions = {
         weekday: "short",
