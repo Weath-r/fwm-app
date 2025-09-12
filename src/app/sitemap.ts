6791f6a7575df1ab5a48a9d66a4d256ae16f2a39
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2025-09-12 07:46:12 +0300

    fix(seo): Sitemaps support for greek language. FWM-103

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,28 +6,59 @@ import { urlStationName } from "@/helpers/createStationName";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const routes: MetadataRoute.Sitemap = [
         {
-            url: BASE_URL,
+            url: `${BASE_URL}/en`,
             lastModified: new Date(),
             changeFrequency: "hourly",
             priority: 1,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/el`,
+                },
+            },
         },
         {
-            url: `${BASE_URL}/stations`,
+            url: `${BASE_URL}/en/stations`,
             lastModified: new Date(),
             changeFrequency: "hourly",
             priority: 0.8,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/el/stations`,
+                },
+            },
         },
         {
-            url: `${BASE_URL}/warnings`,
+            url: `${BASE_URL}/en/warnings`,
             lastModified: new Date(),
             changeFrequency: "daily",
             priority: 0.5,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/el/warnings`,
+                },
+            },
         },
         {
-            url: `${BASE_URL}/about-us`,
+            url: `${BASE_URL}/en/fthiotida-forecast`,
+            lastModified: new Date(),
+            changeFrequency: "daily",
+            priority: 0.8,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/el/fthiotida-forecast`,
+                },
+            },
+        },
+        {
+            url: `${BASE_URL}/en/about-us`,
             lastModified: new Date(),
             changeFrequency: "yearly",
             priority: 0.5,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/el/about-us`,
+                },
+            },
         }
     ];
 
@@ -35,10 +66,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     try {
         const response = await dataService.fetchWeatherStations();
         const sitemapEntries: MetadataRoute.Sitemap = response.map((station) => ({
-            url: `${BASE_URL}/station/${station.id}/${urlStationName(station.name)}`,
+            url: `${BASE_URL}/en/station/${station.id}/${urlStationName(station.name)}`,
             lastModified: new Date(),
             changeFrequency: "hourly",
-            priority: 0.9,
+            priority: 0.8,
+            alternates: {
+                languages: {
+                    el: `${BASE_URL}/en/station/${station.id}/${urlStationName(station.name)}`,
+                },
+            },
         }));
 
         routes.push(...sitemapEntries);
