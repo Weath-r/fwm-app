7552feae55831ef200621cd9bf842a9fd043918a
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2026-07-21 08:56:01 +0300

    fix(homepage): Fix ISR issue with page

--- src/app/[lng]/(home)/page.tsx
+++ src/app/[lng]/(home)/page.tsx
@@ -1,5 +1,7 @@
 import Homepage from "./page.homepage";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home({ params }: { params: Promise<{ lng: string }> }) {
     const { lng } = await params;
     return <Homepage lng={lng} />;
