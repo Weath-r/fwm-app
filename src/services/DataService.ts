c09ab073df4589d9bb693d44109f77fcae85ef36
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2026-08-11 10:00:52 +0300

    refactor(forecast): Update forecast to match the new structure. FWM-180 (#238)

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -387,7 +387,7 @@ export class DataService {
         const filter = this.buildForecastByStationFilter(station_id);
         return this.fetchWithValidation<WeatherForecastResponse[]>(
             filter,
-            WeatherForecastDataResponsesSchema as any
+            WeatherForecastDataResponsesSchema
         );
     };
 
